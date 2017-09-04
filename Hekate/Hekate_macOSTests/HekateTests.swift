5b600fea724ad0f8a60cc7783ac2d2f5a1a011c3
Hannes Oud <hannes.oud.dev@gmail.com>
2017-09-04 17:00:27 +0200

    Implement extraction of receipt payload from the container, add tests

--- Hekate/Hekate_macOSTests/HekateTests.swift
+++ Hekate/Hekate_macOSTests/HekateTests.swift
@@ -6,10 +6,53 @@
 //  Copyright © 2017 IdeasOnCanvas GmbH. All rights reserved.
 //
 
+import Hekate
 import XCTest
 
+//swiftlint:disable force_try
 class HekateTests: XCTestCase {
-    func testNothingReally() {
-        print("Hello world")
+    func testDecodePayloadFromContainerSuccess() {
+        _ = assertDecodePayload(filename: "hannes_mac_mindnode_receipt")
+        _ = assertDecodePayload(filename: "hannes_mac_mindnode_pro_receipt")
+    }
+
+    func testDecodePayloadFromContainerFailure() {
+        guard let error = assertDecodePayloadFailure(filename: "not_a_receipt") else {
+            return
+        }
+        guard let hekateError = error as? HekateError else {
+            XCTFail("Unexpected error type \(error)")
+            return
+        }
+        XCTAssertEqual(hekateError, HekateError.failedToUpdateCMSDecoder)
+    }
+}
+
+
+extension XCTestCase {
+    func assertDecodePayload(filename: String, file: StaticString = #file, line: UInt = #line) -> Data? {
+        guard let data = assertTestAsset(filename: filename) else { return nil }
+        do {
+            let decoded = try Hekate.decodeASN1Payload(receiptPKCS7ContainerData: data)
+            XCTAssertNotNil(decoded)
+            if decoded.isEmpty {
+                XCTFail("Found empty Payload", file: file, line: line)
+            }
+            return decoded
+        } catch {
+            XCTFail("Error when decoding payload from receipt container: \(error)", file: file, line: line)
+            return nil
+        }
+    }
+
+    func assertDecodePayloadFailure(filename: String, file: StaticString = #file, line: UInt = #line) -> Error? {
+        guard let data = assertTestAsset(filename: filename) else { return nil }
+        do {
+            let decoded = try Hekate.decodeASN1Payload(receiptPKCS7ContainerData: data)
+            XCTFail("Expected error decoding the palyoad, but found none, instead found decoded data \(decoded)", file: file, line: line)
+            return nil
+        } catch {
+            return error
+        }
     }
 }
