42babd05772ec3fc5c2b119dd241c756426795b9
Hannes Oud <hannes.oud.dev@gmail.com>
2017-09-05 11:45:27 +0200

    Remove security framework based implementation and tests

--- Hekate/Hekate/Hekate.swift
+++ Hekate/Hekate/Hekate.swift
@@ -7,35 +7,5 @@
 //
 
 import Foundation
-import Security
 
-public struct Hekate {
-    /// Extracts the actual receipt payload data, which is in ASN1-DER binary format, from the PKCS7 receipt container, using CMSDecoder.
-    public static func decodeASN1Payload(receiptPKCS7ContainerData data: Data) throws -> Data {
-        // Inspired by https://github.com/mbogh/Janitor/blob/master/Janitor/ProvisioningProfile.swift and SSWReceiptValidation.m
-        var optionalDecoder: CMSDecoder?
-        guard CMSDecoderCreate(&optionalDecoder) == errSecSuccess,
-            let decoder = optionalDecoder else {
-            throw HekateError.failedToCreateCMSDecoder
-        }
-        guard data.withUnsafeBytes({ bytes -> OSStatus in
-            CMSDecoderUpdateMessage(decoder, bytes, data.count)
-        }) == errSecSuccess else {
-            throw HekateError.failedToUpdateCMSDecoder
-        }
-        CMSDecoderFinalizeMessage(decoder)
-        var optionalPayloadData: CFData?
-        guard CMSDecoderCopyContent(decoder, &optionalPayloadData) == errSecSuccess,
-            let payloadData = optionalPayloadData else {
-            throw HekateError.failedToCopyDataFromCMSDecoder
-        }
-        return payloadData as Data
-    }
-}
-
-public enum HekateError: Int, Error {
-    case unknown
-    case failedToCreateCMSDecoder
-    case failedToUpdateCMSDecoder
-    case failedToCopyDataFromCMSDecoder
-}
+public struct Hekate {}
