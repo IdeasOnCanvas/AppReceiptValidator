bfb520056a27028de9f60f5fdf5fa52eb933da82
Hannes Oud <hannes.oud.dev@gmail.com>
2017-09-07 10:01:22 +0200

    Add sourcery

---
+++ Hekate/Sourcery/Protocols/AutoDiffable.swift
@@ -0,0 +1,9 @@
+//
+//  AutoDiffable.swift
+//  Hekate iOS
+//
+//  Created by Hannes Oud on 07.09.17.
+//  Copyright © 2017 IdeasOnCanvas GmbH. All rights reserved.
+//
+
+protocol AutoDiffable {}
---
+++ Hekate/Sourcery/Protocols/AutoEquatable.swift
@@ -0,0 +1,8 @@
+//
+//  AutoEquatable.swift
+//  
+//
+//  Created by Hannes Oud on 07.09.17.
+//
+
+protocol AutoEquatable {}
