2b83d347941b10ca11388a2f5f9dd0ea97af972d
Hannes Oud <hannes.oud.dev@gmail.com>
2017-09-11 12:18:32 +0200

    Make StoreKitHelper ready for copy paste into existing objc projects

--- Hekate/Hekate Demo iOS/StoreKitHelper.swift
+++ Hekate/Hekate Demo iOS/StoreKitHelper.swift
@@ -9,28 +9,63 @@
 import Foundation
 import StoreKit
 
+@objc
 final class StoreKitHelper: NSObject {
-    var refreshCompletedAction: ((Error?) -> Void)?
 
-    func refresh() {
+    @objc static let shared = StoreKitHelper()
+
+    @objc var refreshCompletedAction: ((NSError?) -> Void)?
+
+    private lazy var delegateHolder: DelegateHolder = {
+        let delegateHolder = DelegateHolder()
+        delegateHolder.refreshCompletedAction = { [weak self] error in
+            self?.refreshCompletedAction?(error)
+        }
+        return delegateHolder
+    }()
+
+    public func refresh() {
         let request = SKReceiptRefreshRequest(receiptProperties: nil)
-        request.delegate = self
+        request.delegate = delegateHolder
         request.start()
+    }
+
+    public func logReceipt() {
+        print("Local Device ID (GUID):\n" + (UIDevice.current.identifierForVendor?.uuidString ?? "nil"))
+        guard let data = receiptData else {
+            print("No receipt")
+            return
+        }
+        let base64 = data.base64EncodedString()
+        print("ReceiptData:\n" + base64)
+    }
+
+    public var receiptData: Data? {
+        guard let url = Bundle.main.appStoreReceiptURL else {
+            return nil
+        }
+        guard let data = try? Data(contentsOf: url) else {
+            return nil
+        }
+        return data
     }
 }
 
-extension StoreKitHelper: SKRequestDelegate {
+/// Encapsules SKRequestDelegate so it is not exposed if not necessary
+private final class DelegateHolder: NSObject, SKRequestDelegate {
+    var refreshCompletedAction: ((NSError?) -> Void)?
+
     func requestDidFinish(_ request: SKRequest) {
-        print("SKReceiptRefreshRequest finished")
+        print("Fetched receipt.")
         DispatchQueue.main.async {
             self.refreshCompletedAction?(nil)
         }
     }
 
     func request(_ request: SKRequest, didFailWithError error: Error) {
-        print("SKReceiptRefreshRequest failed: \(error.localizedDescription)")
+        print("SKReceipt fetching failed: \(error.localizedDescription)")
         DispatchQueue.main.async {
-            self.refreshCompletedAction?(error)
+            self.refreshCompletedAction?(error as NSError)
         }
     }
 }
