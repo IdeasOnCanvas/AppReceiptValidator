db9a6728406521eb88b9ec7198ea3979741b701a
Hannes Oud <hannes.oud.dev@gmail.com>
2017-09-13 19:22:25 +0200

    Change mac framework target to use modulemap
    - make all headers private headers to the target
    - remove bridging header

--- Hekate/Hekate/Hekate-Bridging-Header.h
+++
@@ -1,9 +0,0 @@
-//
-//  Use this file to import your target's public headers that you would like to expose to Swift.
-//
-
-#import "pkcs7.h"
-#import "objects.h"
-#import "sha.h"
-#import "x509.h"
-#import "pkcs7_union_accessors.h"
