7237a8e381e057e2b744dbd860b4387f9c7c884e
Hannes Oud <hannes.oud.dev@gmail.com>
2017-09-13 14:58:19 +0200

    Make project local openssl headers local

--- Hekate/Hekate/Hekate-Bridging-Header.h
+++ Hekate/Hekate/Hekate-Bridging-Header.h
@@ -2,8 +2,8 @@
 //  Use this file to import your target's public headers that you would like to expose to Swift.
 //
 
-#import <openssl/pkcs7.h>
-#import <openssl/objects.h>
-#import <openssl/sha.h>
-#import <openssl/x509.h>
+#import "pkcs7.h"
+#import "objects.h"
+#import "sha.h"
+#import "x509.h"
 #import "pkcs7_union_accessors.h"
