343c555e002b130d8f4b4b44895140606935176d
Michael Schwarz <michael_schwarz@me.com>
2020-01-28 21:03:59 +0100

    Add Vapor Crypto library

--- Package.swift
+++ Package.swift
@@ -10,8 +10,11 @@ let package = Package(
     products: [
         .library(name: "AppReceiptValidator", targets: ["AppReceiptValidator"])
     ],
+    dependencies: [
+        .package(url: "https://github.com/vapor/open-crypto.git", from: "3.3.0")
+    ],
     targets: [
-        .target(name: "AppReceiptValidator", dependencies: []),
+        .target(name: "AppReceiptValidator", dependencies: ["Crypto"]),
         .testTarget(name: "AppReceiptValidatorTests", dependencies: ["AppReceiptValidator"])
     ],
     swiftLanguageVersions: [.v5]
