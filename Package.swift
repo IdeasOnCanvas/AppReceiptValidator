d2e11b3c39bd946d5009209550783adc4cfb3d12
Michael Schwarz <michael_schwarz@me.com>
2020-01-28 20:51:17 +0100

    Add package.swift

---
+++ Package.swift
@@ -0,0 +1,18 @@
+// swift-tools-version:5.0
+import PackageDescription
+
+
+let package = Package(
+    name: "AppReceiptValidator",
+    platforms: [
+        .macOS(.v10_11), .iOS(.v9), .linux
+    ],
+    products: [
+        .library(name: "AppReceiptValidator", targets: ["AppReceiptValidator"])
+    ],
+    targets: [
+        .target(name: "AppReceiptValidator", dependencies: []),
+        .testTarget(name: "AppReceiptValidatorTests", dependencies: ["AppReceiptValidator"])
+    ],
+    swiftLanguageVersions: [.v5]
+)
