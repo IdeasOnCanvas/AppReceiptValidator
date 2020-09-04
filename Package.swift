13dbb9eb1d4e5d8607a576aafabd41ed31946a1f
Artur Grigor <arturgrigor@gmail.com>
2020-09-04 15:38:42 +0300

    Updated the SPM manifest to include the OpenSSL xcframework.

--- Package.swift
+++ Package.swift
@@ -10,14 +10,18 @@ let package = Package(
         .tvOS(.v11)
     ],
     products: [
-        .library(name: "AppReceiptValidator", targets: ["AppReceiptValidator"])
+        .library(name: "AppReceiptValidator", targets: ["AppReceiptValidator", "OpenSSL"])
     ],
     targets: [
         .target(
             name: "AppReceiptValidator",
             path: "AppReceiptValidator/AppReceiptValidator",
             exclude: ["AppReceiptValidator/AppReceiptValidator/Supporting Files/Info.plist"]
-        )
+        ),
+        .binaryTarget(
+            name: "OpenSSL",
+            path: "AppReceiptValidator/AppReceiptValidator/OpenSSL/openssl.xcframework"
+        ),
     ],
     swiftLanguageVersions: [.v5]
 )
