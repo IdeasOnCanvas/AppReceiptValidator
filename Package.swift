329f429fe4ec0641b655013739e80187dc09abd1
Michael Schwarz <michael_schwarz@me.com>
2021-03-17 15:24:39 +0100

    Update Package dependencies

--- Package.swift
+++ Package.swift
@@ -15,8 +15,8 @@ let package = Package(
     ],
     dependencies: [
 		// We currently can't use semantic versioning because the repo uses invalid version strings (e.g. "1.5" instead of "1.5.0")
-        .package(url: "https://github.com/filom/ASN1Decoder", .exact("1.7.0")),
-        .package(url: "https://github.com/apple/swift-crypto", .exact("1.1.2"))
+        .package(url: "https://github.com/filom/ASN1Decoder", .exact("1.7.1")),
+        .package(url: "https://github.com/apple/swift-crypto", from: "1.1.0")
     ],
     targets: [
         .target(name: "AppReceiptValidator",
