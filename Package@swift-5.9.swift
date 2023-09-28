3e25023d3cefe28742dc1a7aa7542fb0784a8bf2
Hannes Oud <hannes.oud.dev@gmail.com>
2023-09-28 14:29:35 +0200

    Add PrivacyInfo.xcprivacy

---
+++ Package@swift-5.9.swift
@@ -0,0 +1,47 @@
+// swift-tools-version:5.9
+
+import PackageDescription
+
+let package = Package(
+    name: "AppReceiptValidator",
+    platforms: [
+        .macOS(.v10_15),
+        .iOS(.v13),
+        .tvOS(.v13),
+        // .watchOS(.v6) watchOS doesn't have UIDevice.current so we can parse, but not validate hash, also, it cannot run XCTest
+    ],
+    products: [
+        .library(name: "AppReceiptValidator", targets: ["AppReceiptValidator"]),
+        .library(name: "AppReceiptValidatorDynamic", type: .dynamic, targets: ["AppReceiptValidator"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/IdeasOnCanvas/ASN1Decoder", from: "1.8.2"),
+        .package(url: "https://github.com/apple/swift-crypto", from: "1.1.0")
+    ],
+    targets: [
+        .target(name: "AppReceiptValidator",
+                dependencies: ["ASN1Decoder",
+                               .product(name: "Crypto", package: "swift-crypto")],
+                resources: [
+                    .copy("Resources/AppleIncRootCertificate.cer"),
+                    .copy("Resources/PrivacyInfo.xcprivacy")
+                ]
+        ),
+        .testTarget(name: "AppReceiptValidatorTests",
+                    dependencies: ["AppReceiptValidator"],
+                    resources: [
+                        .copy("Resources/deprecatedSinglesTypeExpiredAppleCert_receipt.b64"),
+                        .copy("Resources/grandUnifiedExpiredAppleCert_receipt.b64"),
+                        .copy("Resources/hannes_mac_mindnode_receipt"),
+                        .copy("Resources/hannes_mac_mindnode_2_receipt"),
+                        .copy("Resources/mac_mindnode_rebought_receipt"),
+                        .copy("Resources/mindnode_ios_michaelsandbox_receipt1.b64"),
+                        .copy("Resources/mindnode_ios_michaelsandbox_receipt2.b64"),
+                        .copy("Resources/purchasing_experiments_sandbox_receipt.b64"),
+                        .copy("Resources/not_a_receipt"),
+                        .copy("Resources/mindnode_mac_sha256_receipt"),
+                        .copy("Resources/frank4dd-cacert.der")
+                    ])
+    ],
+    swiftLanguageVersions: [.v5]
+)
